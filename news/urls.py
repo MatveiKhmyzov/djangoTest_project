12219a10110f8d99c1ba8b9040de72dbf0f53cd0
MatveiKhmyzov <matw.hmizov@yandex.ru>
2022-03-02 15:11:08 +0300

    Last actions, add news

--- news/urls.py
+++ news/urls.py
@@ -5,4 +5,6 @@ from news.models import Articles
 urlpatterns = [
     re_path(r'^$', ListView.as_view(queryset=Articles.objects.all().order_by("-date")[:20],
                                     template_name="news/posts.html")),
+    re_path(r'^(?P<pk>\d+)$', DetailView.as_view(model = Articles, template_name
+    = "news/post.html"))
 ]
